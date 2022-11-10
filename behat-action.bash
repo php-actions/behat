065cd3b0b18d5b9c6a96a6360fd77756b6418716
Greg Bowler <greg.bowler@g105b.com>
2022-11-10 12:23:52 +0000

    wip: initial action shell script

---
+++ behat-action.bash
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+github_action_path=$(dirname "$0")
+docker_tag=$(cat ./docker_tag)
+echo "Docker tag: $docker_tag" >> output.log 2>&1
+
+if [ -z "$ACTION_BEHAT_PATH" ]
+then
+	phar_url="https://www.getrelease.download/Behat/Behat/$ACTION_VERSION/phar"
+	phar_path="${github_action_path}/behat.phar"
+	curl --silent -H "User-agent: cURL (https://github.com/php-actions)" -L "$phar_url" > "$phar_path"
+else
+	phar_path="${GITHUB_WORKSPACE}/$ACTION_BEHAT_PATH"
+fi
+
+chmod +x "$phar_path"
+command_string=("behat")
+
+if [ -n "$ACTION_CONFIGURATION" ]
+then
+	command_string+=(--config="$ACTION_CONFIGURATION")
+fi
+
+if [ -n "$ACTION_SUITE" ]
+then
+	command_string+=(--suite="$ACTION_SUITE")
+fi
+
+if [ -n "$ACTION_FORMAT" ]
+then
+	command_string+=(--format="$ACTION_FORMAT")
+fi
+
+if [ -n "$ACTION_OUT" ]
+then
+	command_string+=(--out="$ACTION_OUT")
+fi
+
+if [ -n "$ACTION_NAME" ]
+then
+	command_string+=(--name="$ACTION_NAME")
+fi
+
+if [ -n "$ACTION_TAGS" ]
+then
+	command_string+=(--tags="$ACTION_TAGS")
+fi
+
+if [ -n "$ACTION_ROLE" ]
+then
+	command_string+=(--role="$ACTION_ROLE")
+fi
+
+if [ -n "$ACTION_DEFINITIONS" ]
+then
+	command_string+=(--definitions="$ACTION_DEFINITIONS")
+fi
+
+if [ -n "$ACTION_ARGS" ]
+then
+	command_string+=($ACTION_ARGS)
+fi
+
+if [ -n "$ACTION_PATHS" ]
+then
+	command_string+=("$ACTION_PATHS")
+fi
+
+dockerKeys=()
+while IFS= read -r line
+do
+	dockerKeys+=( $(echo "$line" | cut -f1 -d=) )
+done <<<$(docker run --rm "${docker_tag}" env)
+
+while IFS= read -r line
+do
+	key=$(echo "$line" | cut -f1 -d=)
+	if printf '%s\n' "${dockerKeys[@]}" | grep -q -P "^${key}\$"
+	then
+    		echo "Skipping env variable $key" >> output.log
+	else
+		echo "$line" >> DOCKER_ENV
+	fi
+done <<<$(env)
+
+echo "Command: " "${command_string[@]}" >> output.log 2>&1
+
+docker run --rm \
+	--volume "$phar_path":/usr/local/bin/behat \
+	--volume "${GITHUB_WORKSPACE}":/app \
+	--workdir /app \
+	--env-file ./DOCKER_ENV \
+	--network host \
+	${docker_tag} "${command_string[@]}"
